fb86df2470efde5dea32be28cd878a269a925f74
Jaime Montes de Oca <jaime0422@live.com>
2022-07-28 16:08:38 -0700

    Completed Assignments

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 1, "M", "RED", "Calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Rowdy", 2, "F", "GREEN", "Patient", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Tabitha", 2, "F", "GREEN", "Patient", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Sergeant Brown", 2, "M", "BROWN", "Patient", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Melissa", 2, "F", "BROWN", "Patient", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Grinch", 7, "F", "BROWN", "Agressive", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Wendy", 7, "F", "BROWN", "Agressive", 0);
+
+INSERT INTO bears (age, sex, color, temperament, alive)
+    VALUES (7, "F", "???", "Unknown", 0);
+
